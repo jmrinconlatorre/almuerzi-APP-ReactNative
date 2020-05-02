49ab99ce99a3c09b8ae9ce635391a7ed61ec3e6f
Juan Manuel Rincón Latorre <jm.rincon.latorre@gmail.com>
2020-05-02 12:18:54 +0200

    login and register are working now

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,45 +1,103 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert,
+  AsyncStorage,
+} from "react-native";
+import useForm from "../hooks/useForm";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     height: 60,
-    justifyContent: 'center',
-    borderBottomColor: '#eee',
-    alignItems: 'center',
+    justifyContent: "center",
+    borderBottomColor: "#eee",
+    alignItems: "center",
     borderBottomWidth: 1,
     paddingHorizontal: 15,
   },
   input: {
     fontSize: 18,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 2,
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
     marginBottom: 10,
     margin: 5,
     paddingHorizontal: 5,
   },
   title: {
-      fontSize: 24,
-      marginBottom: 16,
+    fontSize: 24,
+    marginBottom: 16,
   },
   boton: {
-      margin: 10,
-      alignItems: 'center',
-      color: '#fff',
-  }
-
+    margin: 10,
+    alignItems: "center",
+    color: "#fff",
+  },
 });
 
 export default ({ navigation }) => {
+  const initialState = {
+    email: "",
+    password: "",
+  };
+
+  const onSubmit = async (values) => {
+    await fetch("https://serverless.jmrinconlatorre.now.sh/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "Application/json",
+      },
+      body: JSON.stringify(values),
+    })
+      .then((x) => x.text())
+      .then((x) => {
+        try {
+          return JSON.parse(x);
+        } catch {
+          throw x;
+        }
+      })
+      .then((x) => {
+        AsyncStorage.setItem("token", x.token);
+        navigation.navigate("Meals");
+      })
+      .catch((e) => Alert.alert("Error", e));
+  };
+
+  const { subscribe, inputs, handleSubmit } = useForm(initialState, onSubmit);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Iniciar sesión</Text>
-      <TextInput style={styles.input} placeholder='Email'/>
-      <TextInput style={styles.input} placeholder='Password'/>
-      <Button style={styles.boton} title='Iniciar sesión'/>
-      <Button style={styles.boton} title='Registrarse' onPress={() => navigation.navigate('Register') }/>
+      <TextInput
+        value={inputs.email}
+        autoCapitalize='none'
+        onChangeText={subscribe("email")}
+        style={styles.input}
+        placeholder='Email'
+      />
+      <TextInput
+        autoCapitalize='none'
+        value={inputs.password}
+        onChangeText={subscribe("password")}
+        style={styles.input}
+        placeholder='Password'
+      />
+      <Button
+        style={styles.boton}
+        title='Iniciar sesión'
+        onPress={handleSubmit}
+      />
+      <Button
+        style={styles.boton}
+        title='Registrarse'
+        onPress={() => navigation.navigate("Register")}
+      />
     </View>
   );
-}
+};
--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -1,45 +1,90 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
+import useForm from "../hooks/useForm";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     height: 60,
-    justifyContent: 'center',
-    borderBottomColor: '#eee',
-    alignItems: 'center',
+    justifyContent: "center",
+    borderBottomColor: "#eee",
+    alignItems: "center",
     borderBottomWidth: 1,
     paddingHorizontal: 15,
   },
   input: {
     fontSize: 18,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 2,
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
     marginBottom: 10,
     margin: 5,
     paddingHorizontal: 5,
   },
   title: {
-      fontSize: 24,
-      marginBottom: 16,
+    fontSize: 24,
+    marginBottom: 16,
   },
   boton: {
-      margin: 10,
-      alignItems: 'center',
-      color: '#fff',
-  }
-
+    margin: 10,
+    alignItems: "center",
+    color: "#fff",
+  },
 });
 
 export default ({ navigation }) => {
+  const initialState = {
+    email: "",
+    password: "",
+  };
+
+  const onSubmit = (values) => {
+    fetch("https://serverless.jmrinconlatorre.now.sh/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-type": "Application/json",
+      },
+      body: JSON.stringify(values),
+    })
+      .then((x) => x.text())
+      .then((x) => {
+        if (x === "Usuario creado con éxito") {
+          return Alert.alert("Exito", x, [
+            {
+              text: "Ir al inicio",
+              onPress: () => navigation.navigate("Login"),
+            },
+          ]);
+        }
+        Alert.alert("Error", x);
+      });
+  };
+
+  const { subscribe, inputs, handleSubmit } = useForm(initialState, onSubmit);
+  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Registro</Text>
-      <TextInput style={styles.input} placeholder='Email'/>
-      <TextInput style={styles.input} placeholder='Password'/>
-      <Button style={styles.boton} title='Registrarse' />
-      <Button style={styles.boton} title='Volver al inicio' onPress={() => navigation.navigate('Login') }/>
+      <TextInput
+        autoCapitalize='none'
+        onChangeText={subscribe("email")}
+        value={inputs.email}
+        style={styles.input}
+        placeholder='Email'
+      />
+      <TextInput
+        autoCapitalize='none'
+        onChangeText={subscribe("password")}
+        style={styles.input}
+        value={inputs.password}
+        placeholder='Password'
+      />
+      <Button style={styles.boton} title='Enviar' onPress={handleSubmit} />
+      <Button
+        style={styles.boton}
+        title='Volver al inicio'
+        onPress={() => navigation.navigate("Login")}
+      />
     </View>
   );
-}
+};
